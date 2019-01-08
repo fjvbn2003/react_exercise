65eea603567e3b7db2bb3f13291e3a7ca293c8d9
Youngju KIM <fjvbn2003@gmail.com>
2019-01-08 20:54:22 +0900

    three ways to express conditional output

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -2,18 +2,42 @@ import React from 'react';
 
 const Ninjas = ({ninjas}) => {
         //console.log(this.props);
-        const ninjaList = ninjas.map(ninja => {
-            return (            
-            <div className="ninja" key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt:{ninja.belt}</div>
-            </div>
-            )
-        });
+        // const ninjaList = ninjas.map(ninja => {
+        //     if(ninja.age >24){
+        //         return (            
+        //             <div className="ninja" key={ninja.id}>
+        //                 <div>Name: {ninja.name}</div>
+        //                 <div>Age: {ninja.age}</div>
+        //                 <div>Belt:{ninja.belt}</div>
+        //             </div>
+        //             )
+        //     }
+        //     else{ return null}
+           
+        // });
+        // const ninjaList = ninjas.map(ninja => {
+        //     return ninja.age>24 ? (
+        //         <div className="ninja" key={ninja.id}>
+        //             <div>Name: {ninja.name}</div>
+        //             <div>Age: {ninja.age}</div>
+        //             <div>Belt:{ninja.belt}</div>
+        //         </div>
+        //     ) : null;
+        // });
+
         return(
             <div className="ninja-list">
-                {ninjaList}
+                {
+                    ninjas.map(ninja => {
+                         return ninja.age>24 ? (
+                            <div className="ninja" key={ninja.id}>
+                                <div>Name: {ninja.name}</div>
+                                <div>Age: {ninja.age}</div>
+                                <div>Belt:{ninja.belt}</div>
+                                </div>
+                        ) : null;
+                })
+                }
             </div>
         )
 }
