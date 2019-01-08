5991f7f9d5e122cbe3bcf930a876bbba6506ae27
Youngju KIM <fjvbn2003@gmail.com>
2019-01-08 17:34:24 +0900

    #12 Nesting Components

---
+++ my-app/src/Ninjas.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import App from './App';
+
+class Ninjas extends Component{
+    render(){
+        return(
+            <div className="ninja">
+                <div>Name: KIM</div>
+                <div>Age: 25</div>
+                <div>Belt: Black</div>
+            </div>
+        )
+
+    }
+
+}
+export default Ninjas;
