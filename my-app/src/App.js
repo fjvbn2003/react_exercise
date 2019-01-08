be856132c48ce33e6ff4fa30030886d55de89bf2
Youngju KIM <fjvbn2003@gmail.com>
2019-01-08 21:20:09 +0900

    #17 create AddNinja.js file.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state= {
@@ -16,7 +17,7 @@ class App extends Component {
         <h1>My first React App</h1>
         <p>Welcome :)</p>
         <Ninjas ninjas={this.state.ninjas}/>
-
+        <AddNinja/>
       </div>
     );
   }
